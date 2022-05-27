50d1388d2f8115f7213ea9f716403c30f66ab185
Beecoder <73896602+Beecoder01@users.noreply.github.com>
2022-05-27 22:15:54 +0530

    Add files via upload

---
+++ test2app/loadData.js
@@ -0,0 +1,41 @@
+// populating contents:
+document.getElementById('root').innerHTML =
+`
+<iframe src="https://vs4apps.blogspot.com/" width="100%"  frameborder="0" id="show"></iframe>
+    <div class="wrapper" id="display">
+        <h1 id="mText">VS Apps</h1>
+        <h4 id="sText">Click to Start App</h4>
+        <button id="btn1" class="button">START</a>
+        <button id="btn2" class="button">RETRY</a>
+    </div>
+`;
+
+//check internet connection:
+if (!isConnect) {
+    console.log(isConnect);
+    document.getElementById("sText").innerText = `No Internet!`;
+    document.getElementById("btn1").style.display = "none";
+    document.getElementById("btn2").style.display = "block";
+    document.getElementById("display").style.display = "block";
+    document.getElementById("show").style.display = "none";
+
+    btn2.addEventListener('click', (e) => {
+        location.reload();
+    });
+}
+
+//btn1 on click
+btn1.addEventListener('click', (e) => {
+    console.log("started");
+    localStorage.setItem("installed", true);
+    location.reload();
+});
+
+//load data on internet connection true:
+if (loadData) {
+    document.getElementById("display").style.display = "none";
+    document.getElementById("show").style.display = "block";
+    console.log(loadData);
+} else {
+    console.log(loadData);
+}
