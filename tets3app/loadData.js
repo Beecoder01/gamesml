88e5e906fbc071d8196b3fee7fd7a889a7fa2cf1
Beecoder <73896602+Beecoder01@users.noreply.github.com>
2022-05-30 13:08:25 +0530

    Update loadData.js

--- tets3app/loadData.js
+++ tets3app/loadData.js
@@ -5,76 +5,63 @@ document.getElementById('root').innerHTML =
     <div class="wrapper" id="display">
         <h1 id="mText">VS Apps</h1>
         <h4 id="sText">Click to Start App</h4>
-        <button id="btn1" class="button">START</button>
-        <button id="btn2" class="button">RETRY</button>
         <button id="addBtn" class="button">A2HS</button>
+        <button id="retry" class="button">RETRY</button>
     </div>
 `;
 
 // def variables:
 const sText = document.getElementById("sText");
-const btn1 = document.getElementById("btn1");
-const btn2 = document.getElementById("btn2");
 const addBtn = document.getElementById("addBtn");
+const retry = document.getElementById("retry");
 const display = document.getElementById("display");
 const disIframe = document.getElementById("disIframe");
 
 // onload:
 // iframe hidden via style.css
-btn1.style.display = "none";
-btn2.style.display = "none";
+retry.style.display = "none";
 
 
 //check internet connection:
 if (!isConnect) {
     console.log("internet:"+ isConnect);
     sText.innerText = `No Internet!`;
-    btn1.style.display = "none";
     addBtn.style.display = "none";
-    btn2.style.display = "block";
+    retry.style.display = "block";
 
-    btn2.addEventListener('click', (e) => {
+    retry.addEventListener('click', (e) => {
         location.reload();
     });
 }
 
-//btn1 on click
-// btn1.addEventListener('click', (e) => {
-//     console.log("started");
-//     localStorage.setItem("installed", true);
-//     location.reload();
-// });
-
-//load data on internet connection true:
-/*
+// load data when 1. "installed" == true in localstorage && internet connection is true:
 if (loadData) {
-    document.getElementById("display").style.display = "none";
-    document.getElementById("disIframe").style.display = "block";
+    //1. show iframe:
+    disIframe.style.display = "block";
+    //2. hide wrapper display:
+    display.style.display = "none";
+    //3. adding extras: loading bg:
     document.getElementsByTagName('body')[0].style.background = 'unset';
     document.getElementsByTagName('body')[0].classList.add('backgroundLoadData');
-    console.log(loadData);
-} else {
-    console.log(loadData);
 }
-*/
 
-// here adding btn3 work:
+// here adding addBtn beforeinstallprompt:
 let pm;
 addBtn.style.display = 'none';
-
 window.addEventListener('beforeinstallprompt', (e)=>{
     // stop chrome 67+:
     e.preventDefault();
     // store e for later use
     pm = e;
     addBtn.style.display = 'block';
-    //btn3 on click
+    //addBtn on click
     addBtn.addEventListener('click', (e) => {
         addBtn.style.display = 'none';
         pm.prompt();
         pm.userChoice.then((result)=>{
             if (result.outcome === 'accepted') {
-                console.log("user installed the app");
+                console.log("installed the app");
+                localStorage.setItem("installed", true);
             }else{
                 console.log("user declined install");
                 alert("user declined install");
@@ -85,4 +72,3 @@ window.addEventListener('beforeinstallprompt', (e)=>{
         });
     });
 });
-
