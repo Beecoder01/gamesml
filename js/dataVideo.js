b0bfefb19100811aeb505c3a89ed6742618fa2eb
Beecoder <73896602+Beecoder01@users.noreply.github.com>
2022-06-01 00:27:38 +0530

    Add files via upload

---
+++ js/dataVideo.js
@@ -0,0 +1,83 @@
+var vData = [
+    {
+        "vid": "Oq4EQJw17rw",
+        "vtitle": "Sonic HedgeHog"
+    },
+    {
+        "vid": "OlA990o2u6o",
+        "vtitle": "Mars: Xenos War"
+    },
+    {
+        "vid": "UtIX7UGV7VA",
+        "vtitle": "The Escape"
+    },
+    {
+        "vid": "ndkQWWPuUwE",
+        "vtitle": "Demon hunter chapter 1"
+    },
+    {
+        "vid": "VgytkufgEkE",
+        "vtitle": "The Invasion"
+    },
+    {
+        "vid": "TFqwkpLMHxA",
+        "vtitle": "Christmas Warrior"
+    },
+    {
+        "vid": "z884cvFknJg",
+        "vtitle": "Halloween jack-o-lantern"
+    },
+    {
+        "vid": "UOfIXcQ6EBg",
+        "vtitle": "Adventure if Viking"
+    },
+    {
+        "vid": "Pov-BUU-dRc",
+        "vtitle": "Pirates attack"
+    },
+    {
+        "vid": "D5HfonORViI",
+        "vtitle": "Temple Deathmatch"
+    },
+    {
+        "vid": "hLwPMump0lA",
+        "vtitle": "Fractal Spacestation"
+    },
+    {
+        "vid": "RXl9Qsm7H84",
+        "vtitle": "Agent 47"
+    },
+    {
+        "vid": "LGbqNScLaWY",
+        "vtitle": "Space station game"
+    },
+    {
+        "vid": "PPSXf2Nqjoo",
+        "vtitle": "KNight and the 7 gates"
+    },
+    {
+        "vid": "ANOYvOv_Xek",
+        "vtitle": "Mission Red Code full action packed..."
+    },
+    {
+        "vid": "rTQafuA7b7o",
+        "vtitle": "Ultimate Flag Defender 2"
+    }
+];
+
+
+// var s="";
+// try {
+//     myobj = data;
+//     for (elements in myobj) {
+//     s = `
+//     {
+//         "vid": "${myobj[elements].vlink}",
+//         "vtitle":"${myobj[elements].vtitle}"
+//     },
+//     `+ s;
+//     }
+//     document.write(s);
+// } catch (error) {
+//     alert("Network Error");
+// }
